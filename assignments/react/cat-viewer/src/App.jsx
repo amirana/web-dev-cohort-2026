02bbecbe7c0ba71d0297e10aaf53c11489292274
Ami Rana <ranaami1223@gmail.com>
2026-07-28 23:09:18 +0530

    feat: update cat viewer app with API integration, improved styling

--- assignments/react/cat-viewer/src/App.jsx
+++ assignments/react/cat-viewer/src/App.jsx
@@ -1,122 +1,209 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cats, setCats] = useState([]);
+  const [page, setPages] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
 
+  useEffect(() => {
+    async function loadCats() {
+      try {
+        const response = await fetch(
+          `https://api.freeapi.app/api/v1/public/cats?page=${page}&limit=1`,
+        );
+        const data = await response.json();
+        setCats(data.data.data);
+        setTotalPages(data.data.totalPages);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
+    loadCats();
+  }, [page]);
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <section className="app">
+        <div className="container">
+          <div className="hero">
+            <h1>Ramdom cat breeds</h1>
+          </div>
+          <div className="cat-list">
+            {cats.map((cat) => {
+              return (
+                <article className="cat-card" key={cat.id}>
+                  <div className="cat-header">
+                    <img src={cat.image} alt={cat.name} className="cat-image" />
+                  </div>
+
+                  <div className="cat-content">
+                    <div className="cat-title">
+                      <h2>{cat.name}</h2>
+                    </div>
+                    <p className="description">{cat.description}</p>
+
+                    <div className="cat-info-grid">
+                      <div className="info-box">
+                        <span>Origin</span>
+                        <strong>{cat.origin}</strong>
+                      </div>
+
+                      <div className="info-box">
+                        <span>Life Span</span>
+                        <strong>{cat.life_span}</strong>
+                      </div>
+
+                      <div className="info-box">
+                        <span>Weight</span>
+                        <strong>{cat.weight.metric} kg</strong>
+                      </div>
+
+                      <div className="info-box">
+                        <span>Temperament</span>
+                        <strong>{cat.temperament}</strong>
+                      </div>
+                    </div>
+
+                    <div className="rating-grid">
+                      <div className="rating">
+                        <div className="rating-top">
+                          <span>🐶 Dog Friendly</span>
+                          <strong>{cat.dog_friendly}/5</strong>
+                        </div>
+                        <div className="progress">
+                          <div
+                            className="progress-fill"
+                            style={{
+                              width: `${cat.dog_friendly * 20}%`,
+                            }}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="rating">
+                        <div className="rating-top">
+                          <span>👶 Child Friendly</span>
+                          <strong>{cat.child_friendly}/5</strong>
+                        </div>
+
+                        <div className="progress">
+                          <div
+                            className="progress-fill"
+                            style={{
+                              width: `${cat.child_friendly * 20}%`,
+                            }}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="rating">
+                        <div className="rating-top">
+                          <span>❤️ Affection</span>
+                          <strong>{cat.affection_level}/5</strong>
+                        </div>
+
+                        <div className="progress">
+                          <div
+                            className="progress-fill"
+                            style={{
+                              width: `${cat.affection_level * 20}%`,
+                            }}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="rating">
+                        <div className="rating-top">
+                          <span>⚡ Energy</span>
+                          <strong>{cat.energy_level}/5</strong>
+                        </div>
+
+                        <div className="progress">
+                          <div
+                            className="progress-fill"
+                            style={{
+                              width: `${cat.energy_level * 20}%`,
+                            }}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="rating">
+                        <div className="rating-top">
+                          <span>🧠 Intelligence</span>
+                          <strong>{cat.intelligence}/5</strong>
+                        </div>
+
+                        <div className="progress">
+                          <div
+                            className="progress-fill"
+                            style={{
+                              width: `${cat.intelligence * 20}%`,
+                            }}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="rating">
+                        <div className="rating-top">
+                          <span>🐾 Social Needs</span>
+                          <strong>{cat.social_needs}/5</strong>
+                        </div>
+
+                        <div className="progress">
+                          <div
+                            className="progress-fill"
+                            style={{
+                              width: `${cat.social_needs * 20}%`,
+                            }}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="links">
+                      <a
+                        href={cat.wikipedia_url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Wikipedia
+                      </a>
+                      <a
+                        href={cat.vetstreet_url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Vetstreet
+                      </a>
+                    </div>
+                  </div>
+                </article>
+              );
+            })}
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
+
+        <div className="page-indicator">
+          <button
+            onClick={() => setPages((page) => page - 1)}
+            disabled={page === 1}
+          >
+            Prev
+          </button>
+          <button
+            onClick={() => setPages((page) => page + 1)}
+            disabled={page === totalPages}
+          >
+            Next
+          </button>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Page {page} of {totalPages}
           </p>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
