29f804d8b232b624b331af704039bf2b5e065566
Ami Rana <ranaami1223@gmail.com>
2026-07-30 18:32:27 +0530

    feat: implement user fetching with pagination and update UI layout in App component

--- assignments/react/random-users/src/App.jsx
+++ assignments/react/random-users/src/App.jsx
@@ -1,117 +1,70 @@
-import { useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+  const [page, setPages] = useState(1)
+  const [totalPage, setTotalPages] = useState(null);
+
+  useEffect(() => {
+    async function loadUsers() {
+      try {
+        const response = await fetch(`https://api.freeapi.app/api/v1/public/randomusers?page=${page}&limit=10`)
+        const data = await response.json()
+
+        setUsers(data.data.data)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    loadUsers()
+  }, [page])
+
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <section id="app">
+        <div className='container'>
+          <div>
+            <h1>Random Users</h1>
+            {users.map(user => (
+              <article key={user.id}>
+                <img src={user.picture.large} alt={user.name.first} />
+                <div>
+                  <h3>{user.name.title}. {user.name.first} {user.name.last}</h3>
+                  <address>
+                    <p>{user.location.street.number}, {user.location.street.name}</p>
+                    <span>{user.location.city}</span>, <span>{user.location.country}</span>
+                    <span> - {user.location.postcode}</span>
+                  </address>
+                </div>
+                <div>
+                  <p>{user.email}</p>
+                  <p>{user.cell}</p>
+                </div>
+              </article>
+              
+            ))}
+          </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+          <div>
+            <button
+              onClick={() => setPages((page) => page - 1)}
+              disabled={page === 1}
+            >
+              Prev
+            </button>
+            <button
+              onClick={() => setPages((page) => page + 1)}
+              disabled={page === totalPage}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
